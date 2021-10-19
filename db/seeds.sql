cb9463eb1b87acba2064714f8d624773c92a3f26
Justin Bedford <jbedford94@gmail.com>
2021-10-18 22:05:52 -0400

    seeds for roles/dept

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO DEPARTMENT (title)
+VALUES
+    ('Legal'),
+    ('Finance'),
+    ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+    ('Sales lead', 60000, 3),
+    ('Accountant', 75000, 2),
+    ('Lawyer', 95000, 1)
+    ('Legal Manager', 99000,1)
+    ('Sales Agent', 35000, 3);
